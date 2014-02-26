efa97078efb57bb89607bde47a921596859b7fe0
Chuck Greb <charles.greb@gmail.com>
2014-02-26 09:59:10 -0500

    Remove cruft from test runner.

--- speakerbox/src/test/java/com/mapzen/speakerbox/SpeakerboxTestRunner.java
+++ speakerbox/src/test/java/com/mapzen/speakerbox/SpeakerboxTestRunner.java
@@ -4,16 +4,7 @@ import org.junit.runners.model.InitializationError;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.bytecode.ShadowMap;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class SpeakerboxTestRunner extends RobolectricTestRunner {
-    private static final List<String> CUSTOM_SHADOW_TARGETS =
-            Collections.unmodifiableList(Arrays.asList(
-                    "com.mapzen.speakerbox.ShadowTextToSpeech"
-            ));
-
     public SpeakerboxTestRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
     }
